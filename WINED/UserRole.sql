b5318a68cc219e146f1d62f810ac33612e9cc5c3
alvivar <andresalvivar@gmail.com>
2018-08-09 17:56:46 -0600

    Users and roles

---
+++ WINED/UserRole.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserRole]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[FK_UserRole_User_Id] INT NOT NULL,
+	[FK_UserRole_Role_Id] INT NOT NULL,
+	CONSTRAINT [FK_UserRole_User] FOREIGN KEY ([FK_UserRole_User_Id]) REFERENCES [User]([Id]),
+	CONSTRAINT [FK_UserRole_Role] FOREIGN KEY ([FK_UserRole_Role_Id]) REFERENCES [Role]([Id])
+)
