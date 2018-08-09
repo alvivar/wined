1300b19394a74b871acbee748e8a18a254a08572
alvivar <andresalvivar@gmail.com>
2018-08-09 11:42:55 -0600

    Recommended food

---
+++ WINED/ReviewFood.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ReviewFood]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FK_ReviewFood_Review_Id] INT NULL, 
+    [FK_ReviewFood_Food_Id] INT NULL, 
+    CONSTRAINT [FK_ReviewFood_Review] FOREIGN KEY ([FK_ReviewFood_Review_Id]) REFERENCES [Review]([Id]), 
+    CONSTRAINT [FK_ReviewFood_Food] FOREIGN KEY ([FK_ReviewFood_Food_Id]) REFERENCES [Food]([Id])
+)
