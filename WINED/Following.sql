560ac655f5fe25a7e2113e4be9fe19d0eeb0dcc5
alvivar <andresalvivar@gmail.com>
2018-08-09 14:15:52 -0600

    Following

--- WINED/Following.sql
+++ WINED/Following.sql
@@ -1,4 +1,9 @@
 ﻿CREATE TABLE [dbo].[Following]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserId] INT NOT NULL, 	
+    [FollowerId] INT NOT NULL, 
+	[IsFollower] BIT NOT NULL,
+    [FollowDate] DATETIME NOT NULL, 
+    [UnfollowDate] DATETIME NOT NULL    
 )
