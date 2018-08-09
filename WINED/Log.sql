18962c533dadd1d5d4c37f6f46278ee67e28f011
alvivar <andresalvivar@gmail.com>
2018-08-09 17:17:09 -0600

    Log table

---
+++ WINED/Log.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Log]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [FK_Log_LogLevel_Id] INT NOT NULL,
+    [FK_Log_LogType_Id] INT NOT NULL,
+    [FK_Log_LogSource_Id] INT NOT NULL,
+    [Description] VARCHAR(1000) NOT NULL,
+    [Computer] VARCHAR(50) NOT NULL,
+    [User] VARCHAR(50) NOT NULL,
+    [Ip] VARCHAR(50) NOT NULL,
+    [Details] VARCHAR(1000) NULL, 
+    [ReferenceId1] INT NULL, 
+    [ReferenceId2] INT NULL, 
+    CONSTRAINT [FK_Log_LogLevel] FOREIGN KEY ([FK_Log_LogLevel_Id]) REFERENCES [LogLevel]([Id]),
+    CONSTRAINT [FK_Log_LogType] FOREIGN KEY ([FK_Log_LogType_Id]) REFERENCES [LogType]([Id]),
+    CONSTRAINT [FK_Log_LogSource] FOREIGN KEY ([FK_Log_LogSource_Id]) REFERENCES [LogSource]([Id])
+)
