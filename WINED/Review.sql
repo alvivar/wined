f5b39a5da636c066fb2426a311cdfc5271d92237
alvivar <andresalvivar@gmail.com>
2018-08-09 17:56:17 -0600

    Review improved

--- WINED/Review.sql
+++ WINED/Review.sql
@@ -1,12 +1,15 @@
 ﻿CREATE TABLE [dbo].[Review]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
-	[FK_Review_User_Id] INT NOT NULL, 
-	[FK_Review_Wine_Id] INT NOT NULL, 
-    [Description] VARCHAR(1000) NOT NULL, 
-    [Rating] DECIMAL NOT NULL,     
-	[PurchasePrice] NUMERIC NOT NULL, 
-    [Location] [sys].[geography] NOT NULL,     
-    CONSTRAINT [FK_Review_User] FOREIGN KEY ([FK_Review_User_Id]) REFERENCES [User]([Id]), 
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [FK_Review_User_Id] INT NOT NULL,
+    [FK_Review_Wine_Id] INT NOT NULL,
+    [Description] VARCHAR(1000) NOT NULL,
+    [Rating] DECIMAL NOT NULL,
+    [PurchasePrice] NUMERIC NOT NULL,
+    [Computer] VARCHAR(50) NOT NULL,
+    [Username] VARCHAR(50) NOT NULL,
+    [Checksum] VARCHAR(50) NOT NULL,
+    [PostTime] DATETIME NOT NULL,
+    CONSTRAINT [FK_Review_User] FOREIGN KEY ([FK_Review_User_Id]) REFERENCES [User]([Id]),
     CONSTRAINT [FK_Review_Wine] FOREIGN KEY ([FK_Review_Wine_Id]) REFERENCES [Wine]([Id])
 )
