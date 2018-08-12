000870d1f9cff20caa79aa716de42b644d6d1bf9
alvivar <andresalvivar@gmail.com>
2018-08-12 13:51:22 -0600

    Fixed

--- WINED/ReviewAddress.sql
+++ WINED/ReviewAddress.sql
@@ -3,6 +3,6 @@
     [Id] INT NOT NULL PRIMARY KEY IDENTITY,
     [FK_ReviewAddress_Review_Id] INT NOT NULL,
     [FK_ReviewAddress_Address_Id] INT NOT NULL,
-    CONSTRAINT [FK_ReviewAddress_Review] FOREIGN KEY ([FK_ReviewAddress_Review_Id]) REFERENCES [User]([Id]),
+    CONSTRAINT [FK_ReviewAddress_Review] FOREIGN KEY ([FK_ReviewAddress_Review_Id]) REFERENCES [Review]([Id]),
     CONSTRAINT [FK_ReviewAddress_Address] FOREIGN KEY ([FK_ReviewAddress_Address_Id]) REFERENCES [Address]([Id])
 )
