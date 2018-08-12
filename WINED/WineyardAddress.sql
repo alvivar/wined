75070f1a614b20d8803dd067c5ce2873126c8e2c
alvivar <andresalvivar@gmail.com>
2018-08-12 13:51:38 -0600

    Fixed

--- WINED/WineyardAddress.sql
+++ WINED/WineyardAddress.sql
@@ -3,6 +3,6 @@
     [Id] INT NOT NULL PRIMARY KEY IDENTITY,
     [FK_WineyardAddress_Wineyard_Id] INT NOT NULL,
     [FK_WineyardAddress_Address_Id] INT NOT NULL,
-    CONSTRAINT [FK_WineyardAddress_Wineyard] FOREIGN KEY ([FK_WineyardAddress_Wineyard_Id]) REFERENCES [User]([Id]),
+    CONSTRAINT [FK_WineyardAddress_Wineyard] FOREIGN KEY ([FK_WineyardAddress_Wineyard_Id]) REFERENCES [Wineyard]([Id]),
     CONSTRAINT [FK_WineyardAddress_Address] FOREIGN KEY ([FK_WineyardAddress_Address_Id]) REFERENCES [Address]([Id])
 )
