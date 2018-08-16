68dbc9cdf8d4e9fff696b973e7b366fc525a512f
alvivar <andresalvivar@gmail.com>
2018-08-16 02:19:29 -0600

    Identity + better names

--- WINED/Address.sql
+++ WINED/Address.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Address]
 (
-    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     [Line1] VARCHAR(50) NOT NULL,
     [Line2] VARCHAR(50) NOT NULL,
     [Line3] VARCHAR(50) NOT NULL,
@@ -8,5 +8,5 @@
     [Province] VARCHAR(50) NOT NULL,
     [Country] VARCHAR(50) NOT NULL,
     [Details] VARCHAR(1000) NOT NULL,
-    [Location] [sys].[geography] NOT NULL
+    [Geography] [sys].[geography] NOT NULL
 )
