c5cb33b9917aae0949f9733d8ee58834a25d7fd3
alvivar <andresalvivar@gmail.com>
2018-08-16 02:19:38 -0600

    Connected to the users

--- WINED/Following.sql
+++ WINED/Following.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[Following]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [UserId] INT NOT NULL, 	
-    [FollowerId] INT NOT NULL, 
+    [FK_Following_User_Id] INT NOT NULL, 	
+    [FK_Following_Follower_Id] INT NOT NULL, 
 	[IsFollower] BIT NOT NULL,
     [FollowDate] DATETIME NOT NULL, 
-    [UnfollowDate] DATETIME NOT NULL    
+    [UnfollowDate] DATETIME NOT NULL, 
+    CONSTRAINT [FK_Following_User] FOREIGN KEY ([FK_Following_User_Id]) REFERENCES [User]([Id]), 
+    CONSTRAINT [FK_Following_Follower] FOREIGN KEY ([FK_Following_Follower_Id]) REFERENCES [User]([Id])    
 )
