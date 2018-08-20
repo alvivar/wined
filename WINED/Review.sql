d6f119fe7f5e8a635454b59b92bf7999c862a575
alvivar <andresalvivar@gmail.com>
2018-08-20 00:43:55 -0600

    Improved

--- WINED/Review.sql
+++ WINED/Review.sql
@@ -1,15 +1,15 @@
 ﻿CREATE TABLE [dbo].[Review]
 (
-    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     [FK_Review_User_Id] INT NOT NULL,
     [FK_Review_Wine_Id] INT NOT NULL,
     [Description] VARCHAR(1000) NOT NULL,
-    [Rating] DECIMAL NOT NULL,
+    [Rating] TINYINT NOT NULL,
     [PurchasePrice] NUMERIC NOT NULL,
     [Computer] VARCHAR(50) NOT NULL,
     [Username] VARCHAR(50) NOT NULL,
-    [Checksum] VARCHAR(50) NOT NULL,
     [PostTime] DATETIME NOT NULL,
+    [Checksum] VARCHAR(50) NOT NULL,
     CONSTRAINT [FK_Review_User] FOREIGN KEY ([FK_Review_User_Id]) REFERENCES [User]([Id]),
     CONSTRAINT [FK_Review_Wine] FOREIGN KEY ([FK_Review_Wine_Id]) REFERENCES [Wine]([Id])
 )
--- WINED/User.sql
+++ WINED/User.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[User]
 (
     [Id] INT NOT NULL PRIMARY KEY IDENTITY,
-    [Username] VARCHAR(50) NOT NULL,
-    [Fullname] VARCHAR(50) NOT NULL,
+    [UserName] VARCHAR(50) NOT NULL,
+    [Name] VARCHAR(50) NOT NULL,
+    [LastName] VARCHAR(50) NOT NULL,
     [Email] VARCHAR(50) NOT NULL,
-    [Checksum] VARCHAR(50) NOT NULL,
-    [Created] DATETIME NOT NULL 
+    [Created] DATETIME NOT NULL
 )
