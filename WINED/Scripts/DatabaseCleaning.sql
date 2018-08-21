32a1c94945c0e7d0f06a6d8b9b38c7d7e6fce3e2
alvivar <andresalvivar@gmail.com>
2018-08-20 23:02:42 -0600

    Clean the review addresses

--- WINED/Scripts/DatabaseCleaning.sql
+++ WINED/Scripts/DatabaseCleaning.sql
@@ -1,4 +1,6 @@
-﻿DELETE FROM [dbo].[WineyardAddress]
+﻿DELETE FROM [dbo].[ReviewAddress]
+DELETE FROM [dbo].[WineyardAddress]
+
 DELETE FROM [dbo].[Review]
 DELETE FROM [dbo].[Wine]
 DELETE FROM [dbo].[Wineyard]
