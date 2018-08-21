4da7bfa6b592696a77669532186df379f65433c7
alvivar <andresalvivar@gmail.com>
2018-08-20 23:01:34 -0600

    Decent identity

--- WINED/ReviewAddress.sql
+++ WINED/ReviewAddress.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[ReviewAddress]
 (
-    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     [FK_ReviewAddress_Review_Id] INT NOT NULL,
     [FK_ReviewAddress_Address_Id] INT NOT NULL,
     CONSTRAINT [FK_ReviewAddress_Review] FOREIGN KEY ([FK_ReviewAddress_Review_Id]) REFERENCES [Review]([Id]),
