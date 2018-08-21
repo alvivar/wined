0fb2b04ab66f5a99884d6dc80d75f169af574536
alvivar <andresalvivar@gmail.com>
2018-08-21 06:09:01 -0600

    Store procedure to get Wines and Wineyards by location

---
+++ WINED/Stored Procedures/getWinesAndWineyards.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[getWinesAndWineyards]
+	@geography GEOGRAPHY
+AS
+
+SELECT Name, Geography
+FROM
+	Wineyard w
+	JOIN WineyardAddress wa ON w.Id = wa.FK_WineyardAddress_Wineyard_Id
+	JOIN Address a ON a.Id = wa.FK_WineyardAddress_Address_Id
+
+RETURN 0
