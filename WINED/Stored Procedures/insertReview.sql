2398a977248113bd1d3ddb9a9f70a4d8c931b9a8
alvivar <andresalvivar@gmail.com>
2018-08-31 02:05:33 -0600

    Insert review work in progress template

---
+++ WINED/Stored Procedures/insertReview.sql
@@ -0,0 +1,32 @@
+﻿CREATE PROCEDURE [dbo].[insertReview]
+	@userId int,
+	@userLocation GEOGRAPHY,
+	@wineId int,
+	@wineName varchar(50),
+	@description varchar(1000),
+	@rating tinyint
+	-- Valorización de atributos del vino
+	-- Comidas de la experiencia del vino
+AS
+
+BEGIN TRANSACTION
+BEGIN TRY
+
+	-- do your SQL statements here
+
+    COMMIT TRANSACTION
+
+END TRY
+
+BEGIN CATCH
+    SELECT
+	ERROR_NUMBER() AS ErrorNumber,
+	ERROR_SEVERITY() AS ErrorSeverity,
+	ERROR_STATE() AS ErrorState,
+	ERROR_PROCEDURE() AS ErrorProcedure,
+	ERROR_LINE() AS ErrorLine,
+	ERROR_MESSAGE() AS ErrorMessage
+    ROLLBACK TRANSACTION
+END CATCH
+
+RETURN 0
