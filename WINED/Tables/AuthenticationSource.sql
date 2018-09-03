6c66617e35e229ff720f45d4a8ff04461b79b0e3
alvivar <andresalvivar@gmail.com>
2018-09-03 07:31:26 -0600

    Identity (1, 1)

--- WINED/Tables/AuthenticationSource.sql
+++ WINED/Tables/AuthenticationSource.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[AuthenticationSource]
 (
-    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
     [Name] VARCHAR(50) NOT NULL,
-    [Description] VARCHAR(50) NOT NULL
+    [Description] VARCHAR(500) NOT NULL
 )
--- WINED/Tables/UserAuthentication.sql
+++ WINED/Tables/UserAuthentication.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[UserAuthentication]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
 	[FK_UserAuthentication_User_Id] INT NOT NULL,
 	[FK_UserAuthentication_Authentication_Id] INT NOT NULL,
 	CONSTRAINT [FK_UserAuthentication_User] FOREIGN KEY ([FK_UserAuthentication_User_Id]) REFERENCES [User]([Id]),
